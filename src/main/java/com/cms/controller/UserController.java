a4b4bd0a019b681deb85b87cbaaf5c9fece70489
zhangshuqing <shu_qingzhang@126.com>
2016-03-21 11:16:52 +0800

    add charset encoding

--- src/main/java/com/cms/controller/UserController.java
+++ src/main/java/com/cms/controller/UserController.java
@@ -8,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.cms.bean.Student;
 import com.cms.bean.User;
 import com.cms.dao.StudentDao;
@@ -37,6 +40,20 @@ public class UserController {
 		model.put("students", students);
 		return new ModelAndView("student", model);
 	}
+	
+	@RequestMapping(value="getStudent.do",produces = "text/plain;charset=UTF-8")
+	@ResponseBody()
+	public String getStudent(HttpServletRequest request){
+		List<Student> students=studentDao.queryAll();
+		JSONObject result=new JSONObject();
+		result.put("result", true);
+		result.put("value", JSON.toJSON(students));
+		return result.toJSONString();
+	}
+	
+	
+	
+	
 	
 
 }
